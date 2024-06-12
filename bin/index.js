2a157f3ce98fd612309a8f70d25d93fad1486aee
jessie717 <jessiezly717@gmail.com>
2024-06-12 18:01:45 +0800

    feat: 删除冗余代码

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,6 @@
 #! /usr/bin/env node
 
 import { Command } from 'commander'
-import chalk from 'chalk'
 
 import { create } from '../lib/create.js'
 // import pkg from '../package.json' assert { type: 'json' }
@@ -20,11 +19,8 @@ program
 	.description('create a new project')
 	.option('-f, --force', '是否强制覆盖已有项目')
 	.action((name, options) => {
-		console.log(chalk.green('项目名称: '), name)
-		console.log(chalk.green('options: '), options)
 		create(name, options)
 	})
 
 // console.log('process argv :>> ', process.argv);
 program.parse(process.argv)
-
